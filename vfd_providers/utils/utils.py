ac250861884b38049759b4f8c9545f976fb47e36
av-dev2 <emgani@aakvatech.com>
2025-02-10 23:54:41 +0300

    feat: update generate_tra_vfd to support multiple VFD providers for posting fiscal receipts

--- vfd_providers/utils/utils.py
+++ vfd_providers/utils/utils.py
@@ -1,17 +1,23 @@
 import click
 import frappe
 from frappe import _
+from vfd_providers.vfd_providers.doctype.vfdplus_settings.vfdplus_settings import post_fiscal_receipt as vfdplus_post_fiscal_receipt
+from vfd_providers.vfd_providers.doctype.total_vfd_setting.total_vfd_setting import post_fiscal_receipt as total_vfd_post_fiscal_receipt
+from vfd_providers.vfd_providers.doctype.simplify_vfd_settings.simplify_vfd_settings import post_fiscal_receipt as simplify_vfd_post_fiscal_receipt
 
 
 @frappe.whitelist()
 def generate_tra_vfd(docname, sinv_doc=None, method="POST"):
     if not sinv_doc:
         sinv_doc = frappe.get_doc("Sales Invoice", docname)
+    
     if sinv_doc.is_not_vfd_invoice or sinv_doc.vfd_status in ["Sent", "Success"]:
         return
+    
     comp_vfd_provider = frappe.get_cached_doc("Company VFD Provider", sinv_doc.company)
     if not comp_vfd_provider:
         return
+    
     vfd_provider = frappe.get_cached_doc("VFD Provider", comp_vfd_provider.vfd_provider)
     if not vfd_provider:
         return
@@ -19,23 +25,23 @@ def generate_tra_vfd(docname, sinv_doc=None, method="POST"):
 
     if not vfd_provider_settings:
         return
+    
     if vfd_provider.name == "VFDPlus":
-        from vfd_providers.vfd_providers.doctype.vfdplus_settings.vfdplus_settings import (
-            post_fiscal_receipt,
-        )
+        vfdplus_post_fiscal_receipt(sinv_doc, method)
+    
     elif vfd_provider.name == "TotalVFD":
-        from vfd_providers.vfd_providers.doctype.total_vfd_setting.total_vfd_setting import (
-            post_fiscal_receipt,
-        )
+        total_vfd_post_fiscal_receipt(sinv_doc, method)
+    
+    elif vfd_provider.name == "SimplifyVFD":
+        simplify_vfd_post_fiscal_receipt(sinv_doc, method)
     else:
         frappe.throw(_("VFD Provider not supported"))
 
-    post_fiscal_receipt(sinv_doc, method)
-
 
 def autogenerate_vfd(doc, method):
-    if doc.is_not_vfd_invoice or doc.vfd_status == "Sent":
+    if doc.is_not_vfd_invoice or doc.vfd_status in ["Sent", "Success"]:
         return
+    
     if doc.is_auto_generate_vfd and doc.docstatus == 1:
         generate_tra_vfd(docname=doc.name, sinv_doc=doc, method=method)
 
